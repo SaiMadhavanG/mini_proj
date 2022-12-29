62ad487049eee97c5d51109c1707383130bb3eb9
Sai Madhavan G <g.saimadhavan@gmail.com>
2022-12-29 23:20:39 +0530

    Shashank Seller in progress

--- demo/shashanklal/shashanklalSeller.java
+++ demo/shashanklal/shashanklalSeller.java
@@ -1,61 +1,86 @@
 package demo.shashanklal;
 
-import ecom.Seller;
+import ecomm.Seller;
+import ecomm.Platform;
+import ecomm.Portal;
+import ecomm.Product;
+import ecomm.Globals;
+import ecomm.Globals.Category;
+import java.util.*;
+import java.time.*;
+import java.util.Random;
+import demo.Book;
+import demo.Mobile;
 
-public class shashanklalSeller extends ecom.Seller {
+public class shashanklalSeller extends ecomm.Seller {
 
     // id is passed in by the class that instantiates sub-type of seller
     public shashanklalSeller(String id) {
         super(id);
+
     }
+
+    generatproducts();
 
     // ID of seller.
     public String getID() {
         return myID;
     }
 
-    // Platform it is being added to.
-    // Should in turn add itself to the Platform (with addSeller)
-    public void addPlatform(Platform thePlatform) {
-        theplatform.addSeller(this);
+    publthePlatform.addSeller(this);}thePlatform){
+
+    One){
+
+    ArrayList<Product> listofproduct = new ArrayList<Product>();for(
+    int i = 0;i<products.size();i++)
+    {
+        if (products[i].getCategory() == whichOne) {
+            listofproduct.add(products[i]);
+        }
+    }return listofproduct;
     }
 
-    // Seller to return listing of Products of specified Category
-    public ArrayList<Product> findProducts(Globals.Category whichOne) {
-        ArrayList<Product> listofproduct = new ArrayList<Product>();
-        for (int i = 0; i < products.size(); i++) {
-            if (products[i].getCategory() == whichOne) {
-                listofproduct.add(products[i]);
+    n buyProduct(String productID, int quantity) {
+
+            for (int i = 0; i < products.size(); i++)
+        {
+            
+            y)
+
+                {                
+                                if (products[i].getproductID() == productID)
+                        
+                    
+                                    if (products[i]
+                    .getuantity() >= quantity) {
+                    if (products[].getCategory() = Category.Mobile) {
+                 
+                      Mobile mb = Mobile(products[i]);
+                     e
+                  mbrn false;.reduceQuantity(quantity);
+                        } else {
+         
+    }
+
+    private void generateproducts() {
+        random.setSeed(LocalTime.now().getNano());
+        int numberofproducts = random.nextInt(2, 100);
+        for (int i = 0; i <= numberofproducts; i++) {
+            if (i % 2 == 0) {
+                int quantity = rand.nextInt(1, 100);
+                float price = rand.nextInt(2, 6) * 10000.0;
+                String name = nameofmobiles.get(rand.nextInt(8));
+                String productID="shashanklal_"+Integer.toString(rand.nextInt(1000))
             }
         }
-        return listofproduct;
     }
 
-    // User wants to buy specified quantity of productID
-    // Return true if transaction succeeds, false otherwise.
-    // Transaction fails if incorrect productID or quantity exceeds available
-    // inventory
-    public boolean buyProduct(String productID, int quantity)
-    {
-        for(int i=0;i<products.size();i++)
-        {
-            if()
-oducts[i].getproductID()==productID
+    Book b = Book(products[i]);
+    private Stri                        b.reduceQuantity(quantity);}
 
-            {
-                if(products[i].getquantity()>=quantity)
-                {
-                    setQuantity(quantity);
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }        }
+    private String[] nameofmobiles = { "Tony", "Ramsung", "Pineapple", "Tivo", "Joppo", "PlusPlus", "Mixel",
+            "Thor" };}}else return false;}}return false;}
 
-        return false;    }
-
-    private String myID;r
-    private ArrayList<Prodpuct> products=new ArrayList<Product>();
+    private String myID;
+    private ArrayList<Product> products = new ArrayList<Product>();
 }
