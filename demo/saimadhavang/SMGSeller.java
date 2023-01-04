da61692d242e6a1591aacc14aad9f4f229912dcd
Sai Madhavan G <g.saimadhavan@gmail.com>
2023-01-04 19:46:21 +0530

    SMGSeller fixed

--- demo/saimadhavang/SMGSeller.java
+++ demo/saimadhavang/SMGSeller.java
@@ -16,19 +16,22 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SMGSeller extends Seller//SMGSeller is the child of Seller class
+public class SMGSeller extends Seller// SMGSeller is the child of Seller class
 {
-    public SMGSeller(String ID)//Constructor for SMGSeller
+    public SMGSeller(String ID)// Constructor for SMGSeller
     {
-        super(ID);//Calling the parent constructor
+        super(ID);// Calling the parent constructor
         sellerID = ID;
         generateProducts();
     }
-    //Initialise the member platform for the respective seller object
+
+    // Initialise the member platform for the respective seller object
     public void addPlatform(Platform platform) {
         this.platform = platform;
     }
-    //This function will return an arraylist of products which are of the desired category
+
+    // This function will return an arraylist of products which are of the desired
+    // category
     public ArrayList<Product> findProducts(Globals.Category whichOne) {
         ArrayList<Product> categoryList = new ArrayList<>();
         for (Product product : productsList) {
@@ -38,9 +41,12 @@ public class SMGSeller extends Seller//SMGSeller is the child of Seller class
         }
         return categoryList;
     }
-    //This fuction will check for the availability of the product desired by the user
-    //It returns true if the requested product is availaible in the desired quantity
-    //Otherwise it returns flase
+
+    // This fuction will check for the availability of the product desired by the
+    // user
+    // It returns true if the requested product is availaible in the desired
+    // quantity
+    // Otherwise it returns flase
     public boolean buyProduct(String productID, int quantity) {
         Product product = productMap.get(productID);
         if (product.getQuantity() >= quantity) {
@@ -56,57 +62,55 @@ public class SMGSeller extends Seller//SMGSeller is the child of Seller class
             return false;
         }
     }
-    //This function will generateproducts, with random quantity,price and name
+
+    // This function will generateproducts, with random quantity,price and name
     private void generateProducts() {
         random.setSeed(LocalTime.now().getNano());
         int numOfProducts = random.nextInt(12);
-        while (numOfProducts > 5) {
-            numOfProducts = random.nextInt(12);
-        }
         Set<String> nameSet = new HashSet<String>();
-
+        System.out.println("Num of products: " + numOfProducts);
         for (int i = 0; i < numOfProducts; i++) {
             LocalTime time = LocalTime.now();
             String productID = sellerID + "-" + Integer.toString(1000 + i);
             if (time.getNano() % 2 == 1) {
                 Category category = Category.Mobile;
                 String name = nameGenerator(category);
-                while (!nameSet.contains(name)) {
+                while (nameSet.contains(name)) {
                     name = nameGenerator(category);
                 }
                 nameSet.add(name);
                 float price = (random.nextInt() * 1000) % 80000;
                 int quantity = random.nextInt(100);
-                Mobile mobile = new Mobile(category, name, productID, price, quantity);//Creating an object of the mobile class 
-                productsList.add(mobile);//Adding the mobile to arraylist of products
-                productMap.put(productID, mobile);//Adding the productID to the hashmap
-            }
-            else
-            {
+                Mobile mobile = new Mobile(category, name, productID, price, quantity);// Creating an object of the
+                                                                                       // mobile class
+                productsList.add(mobile);// Adding the mobile to arraylist of products
+                productMap.put(productID, mobile);// Adding the productID to the hashmap
+            } else {
                 Category category = Category.Book;
                 String name = nameGenerator(category);
-                while (!nameSet.contains(name)) {
+                while (nameSet.contains(name)) {
                     name = nameGenerator(category);
                 }
                 nameSet.add(name);
                 float price = (random.nextInt() * 10) % 2000;
                 int quantity = random.nextInt(100);
-                Book book = new Book(category, name, productID, price, quantity);//Creating an object of the book class
-                productsList.add(book);//Adding the object in the arraylist of products
-                productMap.put(productID, book);//Adding the respective productID to the hashmap
+                Book book = new Book(category, name, productID, price, quantity);// Creating an object of the book class
+                productsList.add(book);// Adding the object in the arraylist of products
+                productMap.put(productID, book);// Adding the respective productID to the hashmap
             }
         }
     }
-    //This function will generate a name for the respective product randomly
+
+    // This function will generate a name for the respective product randomly
     private String nameGenerator(Globals.Category category) {
         if (category == Category.Mobile) {
             String[] names_0 = { "Sansumg", "Appel", "Macrohard", "Mokia", "OneMinus", "Fotorola" };
             String[] names_3 = { "Pro", "Max", "Prime", "Ultra", "Lite", "Plus" };
-            String name_0 = names_0[random.nextInt(6)];//Name_0 is a random name from the array of strings names_0
-            String name_1 = Character.toString((char) (65 + random.nextInt(26)));//It is a random alphabet
-            String name_2 = Integer.toString(random.nextInt(23) + 1);//WIll generate a random integer
-            String name_3 = names_3[random.nextInt(6)];//Name_3 is a random name from the array of strings names_3
-            String name = "SMG_" + name_0 + " " + name_1 + name_2 + " " + name_3;//The final name of the product
+            String name_0 = names_0[random.nextInt(6)];// Name_0 is a random name from the array of strings names_0
+            String name_1 = Character.toString((char) (65 + random.nextInt(26)));// It is a random alphabet
+            String name_2 = Integer.toString(random.nextInt(23) + 1);// WIll generate a random integer
+            String name_3 = names_3[random.nextInt(6)];// Name_3 is a random name from the array of strings names_3
+            String name = "SMG_" + name_0 + " " + name_1 + name_2 + " " + name_3;// The final name of the product
             return name;
         } else if (category == Category.Book) {
             String[] names_0 = { "A Guide to ", "Handbook of ", "Basics of ", "Fundamentals of ", "Essence of ",
@@ -119,7 +123,8 @@ public class SMGSeller extends Seller//SMGSeller is the child of Seller class
                     "Grace ", "Rahul " };
             String[] names_3 = { "Subramaniam", "Brown", "Chattterji", "Parker", "Chokkalingam", "White", "Shah",
                     "Smith", "Gupta", "Rogers" };
-            //A random bookname will be generated by selecting a word randomly from each of the four arrays
+            // A random bookname will be generated by selecting a word randomly from each of
+            // the four arrays
             String name = "SMG_" + names_0[random.nextInt(10)] + names_1[random.nextInt(10)] + "by "
                     + names_2[random.nextInt(10)] + names_3[random.nextInt(10)];
             return name;
@@ -131,8 +136,8 @@ public class SMGSeller extends Seller//SMGSeller is the child of Seller class
 
     public static void main(String[] args) {
         SMGSeller smg = new SMGSeller("123");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(smg.nameGenerator(Category.Book));
+        for (Product p : smg.productsList) {
+            System.out.println(p.getName());
         }
     }
 
