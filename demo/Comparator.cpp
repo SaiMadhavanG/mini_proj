d422c6ba2fa5fb48211272270d9bd151b300fba8
sam002239 <94612342+sam002239@users.noreply.github.com>
2023-01-04 17:26:21 +0530

    Returned boolean
    instead of integer and changed string comparison to compare function

--- demo/Comparator.cpp
+++ demo/Comparator.cpp
@@ -2,17 +2,17 @@
 #include"Product.h"
 #include"Comparator.h"
 using namespace std;
-bool Comparator::SortByPrice(Product *p1,Product *p2)
+bool Comparator::SortByPrice(Product p1,Product p2)
 {
-    if(p1->getPrice()==p2->getPrice())
-        return p1->getProductID()<p2->getProductID();
-    return p1->getPrice()<p2->getPrice();
+    if(p1.getPrice()==p2.getPrice())
+        return (p1.getProductID().compare(p2.getProductID()))<0;
+    return p1.getPrice()<p2.getPrice();
 }
-bool Comparator::SortByName(Product *p1,Product *p2)
+bool Comparator::SortByName(Product p1,Product p2)
 {
-    if(p1->getName()==p2->getName())//Sorting by price incase the names are the same
+    if(p1.getName()==p2.getName())//Sorting by price incase the names are the same
     {
         return SortByPrice(p1,p2);
     }
-    return p1->getName()<p2->getName();
+    return (p1.getName().compare(p2.getName()))<0;
 }
